5fbaa2d75f0733a490b80a271bf413a370f05088
Marcos Paulo de Almeida Lima <marcospaulo.mpa@gmail.com>
2024-05-20 09:07:28 -0300

    criar contexto de api

---
+++ iot-manager-api/src/main/java/com/marcos/iotmanager/api/IotmanagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.iotmanager.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotmanagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotmanagerApiApplication.class, args);
+	}
+
+}
